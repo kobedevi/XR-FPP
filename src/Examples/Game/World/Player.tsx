5b76a9f40bb09da3d5a6965e8db5d7a494d647ca
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2023-07-26 05:06:44 +0200

    improved player moving

--- src/Examples/Game/World/Player.tsx
+++ src/Examples/Game/World/Player.tsx
@@ -5,12 +5,9 @@ import {
 } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { CuboidCollider, RigidBody, vec3 } from "@react-three/rapier";
-import { useRef, useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { Car } from "./Car";
-import { useGameStore } from "./store";
-import { Donut } from "./Donut";
-import { BoxGeometry, Mesh, MeshBasicMaterial } from "three";
-import { GLTFLoader } from "three-stdlib";
+import { BoxGeometry, Euler, Mesh, MeshBasicMaterial, Quaternion } from "three";
 import donut from "./assets/donut.glb";
 
 
@@ -29,6 +26,7 @@ const RapierWorldPlayer = (props: any) => {
   const playerRef = useRef<any>(null!);
   const pointerRef = useRef<any>(null!);
   const modelRef = useRef<any>(null!);
+  const projectileRef = useRef<any>(null!);
 
   const [projectiles, setProjectiles] = useState([]);
 
@@ -55,8 +53,6 @@ const RapierWorldPlayer = (props: any) => {
     // Change rotation of model
     let changeRotation = false;
     
-
-
     const currentTime = window.performance.now();
     const elapsedTimeSinceLastExecution = currentTime - lastExecutionTimeRef.current;
     const timeBetweenExecutions = 500 / maxExecutions;
@@ -73,10 +69,15 @@ const RapierWorldPlayer = (props: any) => {
       // Call the function and increment the counter
       const carPosition = player.translation()
       carPosition['x'] += 1;
-      carPosition['y'] += 1;
+      carPosition['y'] += 1; 
+
       const geometry = new BoxGeometry(.2, .2, .2)
       const material = new MeshBasicMaterial({color: 0xffff00})
       const projectile = new Mesh(geometry, material)
+      if(projectileRef.current) {
+        projectileRef.current.applyImpulse({ x: .3, y: 0, z: 0 }, true);
+      }
+
       // @ts-ignore
       setProjectiles((prev) => [...prev, {projectile, carPosition}]);
       executionCountRef.current++;
@@ -104,40 +105,42 @@ const RapierWorldPlayer = (props: any) => {
     }
     if(changeRotation) {
       const angle = Math.atan2(-linvel.x, -linvel.z);
-      modelRef.current.rotation.y = angle;
+      playerRef.current.setRotation(
+        new Quaternion().setFromEuler(new Euler(0, angle , 0)), true
+      );
     }
   });
 
   const resetPosition = () => {
     playerRef.current.setTranslation(vec3({ x: 0, y: 0, z: 0 }));
-    playerRef.current.setLinvel(vec3({ x: 0, y: 0, z: 0 }));
+    playerRef.current.setLinvel(vec3({ x: 0, y: 0, z: 0 }));    
   };
-
-  let score = 0;
 
   return (
     <>
       {projectiles.map((projectile, index) => {
         const clone = donutScene.clone();
         return (
-        <RigidBody name="projectile" key={index} position={[projectile['carPosition']['x'],projectile['carPosition']['y'],projectile['carPosition']['z']]} >
-          <primitive object={clone}/>
-        </RigidBody>
-      )})}
-      <group name="Player" position={[0, 3, 0]} rotation={[0, Math.PI , 0]} >
+          <RigidBody ref={projectileRef} name="projectile" key={index} position={[projectile['carPosition']['x'],projectile['carPosition']['y'],projectile['carPosition']['z']]} >
+            <primitive object={clone}/>
+          </RigidBody>
+        )
+      })}
+      <group name="Player" position={[0, 3, 0]} rotation={[0, 0 , 0]} >
         <PointerLockControls ref={pointerRef} selector="#start" />
 
-        {/* reset if it touches "void plane" */}
-        <RigidBody ref={playerRef} colliders={false} scale={[.5,.5,.5]} enabledRotations={[false,false,false]} onIntersectionEnter={({other}) => {
+        {/* reset if it touches "void plane" */} 
+        <RigidBody ref={playerRef} colliders={false} scale={[.5,.5,.5]} enabledRotations={[false,false,false]} rotation={[0,0,0]} onIntersectionEnter={({other}) => {
           if(other.rigidBodyObject !== undefined) {
             if(other.rigidBodyObject.name === "void") {
               resetPosition();
             }
           }
         }}>
+
           <CuboidCollider args={[2,4,3.3]} position={[0, 3, 0]} rotation={[0,0,0]}/>
           <group name="model" ref={modelRef}>
-            <Car position={[0, -1, 0]}/>
+            <Car position={[0, -1, 0]} rotation={[0,Math.PI,0]} enabledRotations={[false,false,false]}/>
           </group>
         </RigidBody>
       </group>
