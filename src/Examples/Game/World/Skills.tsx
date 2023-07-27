f2dd645ce10fd88625b87e1b6cb6498643fd6487
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2023-07-27 16:52:20 +0200

    Added more billboards

--- src/Examples/Game/World/Skills.tsx
+++ src/Examples/Game/World/Skills.tsx
@@ -6,13 +6,14 @@ import localFont from "./fonts/font.json";
 // imgs
 import paranormax from './assets/img/paranormax.png'
 import rs from './assets/img/rsMobile.png'
+import motionCam from './assets/img/motionCam.png'
 
 const Skills = () => {
   
   return (
     <>
       {/* @ts-ignore */}
-      <Text3D font={localFont} position={[-15, 5, -10]} bevelSegments={5} scale={.7} >
+      <Text3D font={localFont} position={[-10, 5, -10]} bevelSegments={5} scale={.7} >
         Some of my work!
         <meshStandardMaterial color="#4f5242" />
       </Text3D>
@@ -29,6 +30,13 @@ const Skills = () => {
         title={"RS mobile"}
         text={"A Laravel project to promote a mobile game. It has a full custom back office with a complete CRUD system for home banners, news posts, custom pages, donations, mailinglists and users."}
         url={'http://project.kodev.be/'}
+      />
+      <LocalBillboard 
+        position={[-1,1,-10]}
+        img={motionCam}
+        title={"Motion-Cam"}
+        text={"An IoT project: A Raspberry Pi with a camera detects when theres motion, starts recording saves the clip and sends registered users a push-notification. Created with Python, Firebase and React."}
+        url={'https://youtu.be/blITzTevAjU'}
       />
     </>
   )
